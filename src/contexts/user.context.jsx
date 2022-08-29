593a8cee926bd95ecbfeccbba0dfa0231df8ac91
ashwin alexander <alexwin75@gmail.com>
2022-08-28 22:03:23 -0400

    replacing Context with reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,36 +1,70 @@
-import { createContext } from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
-import { createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
+import { createContext } from "react";
+import { useEffect } from "react";
+import { useReducer } from "react";
+import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 //very similar to a component that wraps around other components
 
 //actual value you want to access : default value, not initial value
 export const UserContext = createContext({
-	currentUser: null,
-	setCurrentUser: () => null
+  currentUser: null,
+  setCurrentUser: () => null
 });
+
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER"
+};
+
+//function that takes initial state, action and returns updated state depending on the action
+const userReducer = (state, action) => {
+  console.log("dispatch");
+  console.log(action, "this is the action");
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload
+      };
+
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null
+};
 
 //functional component with children,
 export const UserProvider = ({ children }) => {
-	const [currentUser, setCurrentUser] = useState(null);
-	const value = { currentUser, setCurrentUser };
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+  console.log(currentUser, "this is the current user");
 
-	//run this function only once when the component mounts (so empty array)
-	//similar to component mount
-	useEffect(() => {
-		const unsubscribe = onAuthStateChangedListener((user) => {
-			if (user) {
-				createUserDocumentFromAuth(user);
-			}
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
 
-			setCurrentUser(user);
-		});
+  const value = { currentUser, setCurrentUser };
 
-		return unsubscribe;
-	}, []);
+  //run this function only once when the component mounts (so empty array)
+  //similar to component mount
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
 
-	//for every context that gets built there is a provider
-	//provider lets you get value and the setter func anyone within the component tree
-	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+      console.log("setting current userr here");
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  //for every context that gets built there is a provider
+  //provider lets you get value and the setter func anyone within the component tree
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './contexts/user.context';
-import { CartProvider } from './contexts/cart.context';
-import { CategoriesProvider } from './contexts/categories.context';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/user.context";
+import { CartProvider } from "./contexts/cart.context";
+import { CategoriesProvider } from "./contexts/categories.context";
 
-import './index.scss';
-import App from './App';
+import "./index.scss";
+import App from "./App";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<UserProvider>
-				<CategoriesProvider>
-					<CartProvider>
-						<App />
-					</CartProvider>
-				</CategoriesProvider>
-			</UserProvider>
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
